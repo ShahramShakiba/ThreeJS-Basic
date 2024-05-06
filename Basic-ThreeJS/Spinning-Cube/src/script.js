6d4587d3d9eca538c62e40627cca78df7f5a077b
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-06 12:21:39 +0330

    Near and Far Concept

--- Basic-ThreeJS/Spinning-Cube/src/script.js
+++ Basic-ThreeJS/Spinning-Cube/src/script.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 //a container for other objects
 const scene = new THREE.Scene();
 
-// add objects to the scene
+//add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: '#e0b90b' });
 
@@ -16,14 +16,14 @@ scene.add(cubeMesh);
 //initialize the camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  30
+  window.innerWidth / window.innerHeight, // actual screen size
+  0.1,  //if U wanna see everything keep it smaller 0.00001 but stick with reasonable numbers like 0.1 or 0.5
+  200
 );
 //so far you can imagine that's camera is inside of the Mesh
 
 //now position the camera to see the Mesh
-camera.position.z = 5;
+camera.position.z = 5; // 5 units or 5 meters away
 
 //initialize the renderer
 const canvas = document.querySelector('canvas.threejs');
@@ -31,11 +31,11 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 
-// set the size of the renderer's output canvas
+//set the size of the renderer's output canvas
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
-/* ---------------- Mesh() ----------------
+/* ========================= Mesh() ==========================
 - Mesh is a fundamental building block for creating 3D objects. 
 
 - It represents the geometry(shape) and material(appearance) of an object in a 3D scene. 
@@ -43,16 +43,17 @@ renderer.render(scene, camera);
 - Meshes are used to define the visual aspects of 3D models such as cubes, spheres, characters, buildings, or any custom shapes you want to render.
 */
 
-/* -------------- PerspectiveCamera() --------------
+/* ======================= PerspectiveCamera() =======================
 - is a type of camera that mimics how the human eye perceives objects in the real world. 
 
 - It creates a sense of depth and distance by rendering objects farther away as smaller than those closer to the camera. 
 
 - This effect is achieved through perspective projection, where objects closer to the camera appear larger and those farther away appear smaller.
 
-? PerspectiveCamera(1-fov, 2-aspect ratio, 3-camera near property, 4-camera far property - anything further from this far property - you won't able to see)
+? PerspectiveCamera(1-fov, 2-aspect ratio, 3-camera near property, 4-camera far property)
 
-1. Field of View (FOV): how much of the scene is visible to the camera. 
+1. Field of View (FOV): Camera frustum vertical field of view.
+  - how much of the scene is visible to the camera. 
   - A wider FOV captures more of the scene, 
   - while a narrower FOV zooms in on specific details. 
   - It is typically measured in "degrees" and can greatly impact the perception of depth and spatial relationships within the rendered image.
@@ -68,4 +69,21 @@ renderer.render(scene, camera);
 4. Camera Far Property: determines the maximum distance from the camera at which objects will be rendered. 
   - Anything beyond this distance will not be visible in the final image. 
   - By adjusting the far property, you can control how far into the scene objects are rendered, optimizing performance and reducing unnecessary rendering of distant objects.
+*/
+
+/* ======================= WebGLRenderer() =======================
+- The WebGLRenderer is a crucial component in Three.js that enables rendering 3D graphics using WebGL, a web standard for high-performance graphics rendering.
+
+- it leverages the power of WebGL to render complex 3D scenes efficiently in a web browser. 
+- WebGL provides direct access to the GPU, allowing for hardware-accelerated rendering, which is essential for achieving smooth and visually appealing 3D graphics.
+*/
+
+
+/* ======================= near & far vs position =======================
+? what is the "far-prop" that I can't see this box?
+- for example: "3" -> anything beyond 3 I can't see it
+  because if we set {far to 3} it means we only can see the box in range of 3 units, however we set the {position to 5} so the camera is more far away from over box
+
+? what is the "near-prop" that I can't see this box?
+- fro example: "5" -> since we set the {position to 5} it means the camera is positioned 5 units away and that's the "center of the object" so if we set {near to 5} we can't see anything but since the object in cubeGeometry sets as 1 by 1 by 1, it still exists within 4.5 units of the actual camera
 */
