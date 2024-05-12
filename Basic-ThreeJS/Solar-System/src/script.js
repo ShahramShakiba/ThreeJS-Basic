59c422ffc8c18927a0cae4332b6ad5b9740bc41d
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-12 22:00:56 +0330

    Planets Array

--- Basic-ThreeJS/Solar-System/src/script.js
+++ Basic-ThreeJS/Solar-System/src/script.js
@@ -2,6 +2,62 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Pane } from 'tweakpane';
 
+const planets = [
+  {
+    name: 'Mercury',
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moon: [],
+  },
+  {
+    name: 'Venus',
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moon: [],
+  },
+  {
+    name: 'Earth',
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moon: [
+      {
+        name: 'Moon',
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: 'Mars',
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moon: [
+      {
+        name: 'Phobos',
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: 'Deimos',
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
+
 /*==================== Initialize the Scene ==================*/
 const scene = new THREE.Scene();
 const pane = new Pane();
@@ -11,28 +67,11 @@ const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sunMaterial = new THREE.MeshBasicMaterial({
   color: 0xd1b01f,
 });
-const earthMaterial = new THREE.MeshBasicMaterial({
-  color: 'blue',
-});
-const moonMaterial = new THREE.MeshBasicMaterial({
-  color: 'white',
-});
 
 /*============== Initialize the Mesh ==============*/
 const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
 sun.scale.setScalar(6);
-
-const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
-earth.position.x = 12;
-earth.scale.setScalar(1.6);
-
-const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
-moon.position.x = 2;
-moon.scale.setScalar(0.4);
-earth.add(moon);
-
 scene.add(sun);
-scene.add(earth);
 
 /*==================== Initialize the Camera ==================*/
 const camera = new THREE.PerspectiveCamera(
@@ -46,20 +85,20 @@ camera.position.y = 5;
 
 /*=================== Initialize the Renderer =================*/
 const canvas = document.querySelector('canvas.threejs');
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+//achieving higher resolution rendering
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRatio);
 
 /*========================= Add Controls ======================*/
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.maxDistance = 200;
 controls.minDistance = 20;
-// controls.dampingFactor = 0.8; //default is "0.05"
-// controls.autoRotate = true;
-
-/*===================== Initialize a Clock ==================*/
-const clock = new THREE.Clock();
 
 /*===================== Add Resize Listener ==================*/
 window.addEventListener('resize', () => {
@@ -70,19 +109,18 @@ window.addEventListener('resize', () => {
 
 /*==================== Render Loop  ==========================*/
 const renderLoop = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // animate Meshes
-  earth.rotation.y += 0.01;
-  earth.position.x = Math.sin(elapsedTime) * 12;
-  earth.position.z = Math.cos(elapsedTime) * 12;
-
-  moon.position.x = Math.sin(elapsedTime) * 2;
-  moon.position.z = Math.cos(elapsedTime) * 2;
-
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderLoop);
 };
-
 renderLoop();
+
+/* 
+? rotate Earth around the Sun
+    earth.position.x = Math.sin(elapsedTime) * 12;
+    earth.position.z = Math.cos(elapsedTime) * 12;
+
+?rotate Moon around the Earth
+    moon.position.x = Math.sin(elapsedTime) * 2;
+    moon.position.z = Math.cos(elapsedTime) * 2;
+*/
