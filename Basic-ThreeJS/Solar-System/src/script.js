db798ab22a775b274947c471496e0f0f65164258
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-13 11:32:24 +0330

    Automating Mesh Generation

--- Basic-ThreeJS/Solar-System/src/script.js
+++ Basic-ThreeJS/Solar-System/src/script.js
@@ -2,6 +2,50 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Pane } from 'tweakpane';
 
+/*==================== Initialize the Scene ==================*/
+const scene = new THREE.Scene();
+const pane = new Pane();
+const textureLoader = new THREE.TextureLoader();
+
+/*============== Initialize the Geometry & Material ==============*/
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+const sunTexture = textureLoader.load('/textures/2k_sun.jpg');
+const sunMaterial = new THREE.MeshBasicMaterial({
+  map: sunTexture,
+});
+
+const mercuryTexture = textureLoader.load('/textures/2k_mercury.jpg');
+const mercuryMaterial = new THREE.MeshStandardMaterial({
+  map: mercuryTexture,
+});
+
+const venusTexture = textureLoader.load('/textures/2k_venus_surface.jpg');
+const venusMaterial = new THREE.MeshStandardMaterial({
+  map: venusTexture,
+});
+
+const earthTexture = textureLoader.load('/textures/2k_earth_daymap.jpg');
+const earthMaterial = new THREE.MeshStandardMaterial({
+  map: earthTexture,
+});
+
+const marsTexture = textureLoader.load('/textures/2k_mars.jpg');
+const marsMaterial = new THREE.MeshStandardMaterial({
+  map: marsTexture,
+});
+
+const moonTexture = textureLoader.load('/textures/2k_moon.jpg');
+const moonMaterial = new THREE.MeshStandardMaterial({
+  map: moonTexture,
+});
+
+/*======= Initialize the Sun Mesh =======*/
+const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
+sun.scale.setScalar(6);
+scene.add(sun);
+
+/*=================== Planets Array ===================== */
 const planets = [
   {
     name: 'Mercury',
@@ -9,7 +53,7 @@ const planets = [
     distance: 10,
     speed: 0.01,
     material: mercuryMaterial,
-    moon: [],
+    moons: [],
   },
   {
     name: 'Venus',
@@ -17,7 +61,7 @@ const planets = [
     distance: 15,
     speed: 0.007,
     material: venusMaterial,
-    moon: [],
+    moons: [],
   },
   {
     name: 'Earth',
@@ -25,7 +69,7 @@ const planets = [
     distance: 20,
     speed: 0.005,
     material: earthMaterial,
-    moon: [
+    moons: [
       {
         name: 'Moon',
         radius: 0.3,
@@ -40,7 +84,7 @@ const planets = [
     distance: 25,
     speed: 0.003,
     material: marsMaterial,
-    moon: [
+    moons: [
       {
         name: 'Phobos',
         radius: 0.1,
@@ -58,20 +102,43 @@ const planets = [
   },
 ];
 
-/*==================== Initialize the Scene ==================*/
-const scene = new THREE.Scene();
-const pane = new Pane();
+//=============== Creating Planet
+const createPlanet = (planet) => {
+  //Create the Mesh
+  const planetMesh = new THREE.Mesh(sphereGeometry, planet.material);
+  //Set the Scale
+  planetMesh.scale.setScalar(planet.radius);
+  //Set the Distance
+  planetMesh.position.x = planet.distance;
 
-/*============== Initialize the Geometry & Material ==============*/
-const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-const sunMaterial = new THREE.MeshBasicMaterial({
-  color: 0xd1b01f,
+  return planetMesh;
+};
+
+//=============== Creating Moon
+const createMoon = (moon) => {
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+  moonMesh.scale.setScalar(moon.radius);
+  moonMesh.position.x = moon.distance;
+
+  return moonMesh;
+};
+
+const planetMeshes = planets.map((planet) => {
+  const planetMesh = createPlanet(planet);
+  scene.add(planetMesh);
+
+  //Loop through each Moon
+  planet.moons.forEach((moon) => {
+    const moonMesh = createMoon(moon);
+    planetMesh.add(moonMesh);
+  });
+
+  return planetMesh;
 });
 
-/*============== Initialize the Mesh ==============*/
-const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
-sun.scale.setScalar(6);
-scene.add(sun);
+/*==================== Initialize the Light ==================*/
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
 /*==================== Initialize the Camera ==================*/
 const camera = new THREE.PerspectiveCamera(
