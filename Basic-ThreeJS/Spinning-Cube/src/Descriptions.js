668e78dc097761df69722e138a2f721800c08432
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-14 22:21:02 +0330

    Update the Content of the Course Descriptions

--- Basic-ThreeJS/Spinning-Cube/src/Descriptions.js
+++ Basic-ThreeJS/Spinning-Cube/src/Descriptions.js
@@ -1,113 +1,228 @@
-/* ========================= Mesh() ==========================
-- Mesh is a fundamental building block for creating 3D objects. 
+/* %%%%%%%%%%%%%%%%%%%%%%%% Three.js %%%%%%%%%%%%%%%%%%%%%%%%%%
+* is a high-level JavaScript library for creating and displaying 3D graphics in web browsers.
 
-- It represents the geometry(shape) and material(appearance) of an object in a 3D scene. 
-
-- Meshes are used to define the visual aspects of 3D models such as cubes, spheres, characters, buildings, or any custom shapes you want to render.
+Purpose :
+- simplifies 3D graphics creation and animation (models, particle systems, lights, camera) without low-level WebGL code knowledge.
 */
 
-/* ======================= PerspectiveCamera() =======================
-- is a type of camera that mimics how the human eye perceives objects in the real world. 
+/* %%%%%%%%%%%%%%%%%% Graphic Processing %%%%%%%%%%%%%%%%%%%%%%
+? What is graphic processing?
+- The process of rendering 3D graphics on a computer screen in real-time 
+- Requires millions of calculations per second
+*/
 
-- It creates a sense of depth and distance by rendering objects farther away as smaller than those closer to the camera. 
+/* %%%%%%%%%%%%%%%%%%%%%%%% GPU %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* stands for Graphic-Processing-Unit
+
+- A special type of "hardware" that can run simple calculations in parallel
+- Designed to handle the demands of rendering 3D graphics
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%%%% WebGL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* WebGL is a JavaScript API for rendering 3D graphics in a web browser using the GPU
+
+- WebGL provides a language to talk to the GPU and instruct it on what to render
+
+- Three.js is built on top of WebGL and implements it under the hood.
+
+- Three.js abstracts the complicated WebGL language into an easy-to-use API allowing for creative focus on projects
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%% Install Three.js %%%%%%%%%%%%%%%%%%%%%%%%
+*1: three.js
+npm install --save three
+
+*2: vite
+npm install --save-dev vite
+or 
+npm create vite@latest
+
+*3: import
+import * as THREE from 'three';
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%%%% Scene %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* It acts as a container that holds everything you want to render in your 3D environment. 
+- The scene serves as the "canvas" on which you can build and manipulate your 3D world.
+
+const scene = new THREE.Scene();
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%%%% Mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* Mesh is a fundamental building block for creating 3D objects. 
+
+- It represents the "Geometry(shape)" and "Material(appearance)" of an object in a 3D scene. 
+
+- Meshes are used to define the visual aspects of 3D models such as cubes, spheres, characters, buildings, or any custom shapes you want to render.
+
+const cubeMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),  // Geometry
+  new THREE.MeshBasicMaterial({ color: 'red' })  // Material
+);
+scene.add(cubeMesh);
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%%% Camera %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* defines the viewpoint from which the scene will be rendered. It determines what will be visible to the viewer and how objects in the scene will be projected onto the screen. 
+
+- There are different types of cameras available in three.js, such as "PerspectiveCamera" and "OrthographicCamera", each offering unique ways to view and interact with the 3D scene.
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  1,
+  100
+);
+/-> move the camera back from the exact position of the Mesh 
+camera.position.z = 5;
+scene.add(camera);
+-> U can add or omit the Camera from some 3D-Objects 
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%%% Renderer %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+*  is responsible for taking the 3D scene and rendering it onto a "2D canvas" for display on the screen.
+
+- It handles tasks like converting 3D objects into 2D images, applying lighting and shading effects, and optimizing performance for smooth rendering.
+
+? add our Canvas-> where we draw our 3D-objects | displaying our image
+in HTML.index:
+<canvas class="threejs"></canvas>
+
+in script.js:
+const canvas = document.querySelector('canvas.threejs')
+
+
+* The "WebGLRenderer" is a crucial component in Three.js that enables rendering 3D graphics using WebGL, a web standard for high-performance graphics rendering.
+
+- it leverages the power of WebGL to render complex 3D scenes efficiently in a web browser. 
+- WebGL provides direct access to the GPU, allowing for hardware-accelerated rendering, which is essential for achieving smooth and visually appealing 3D graphics.
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+/-> define the dimensions of a rendering area
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera);
+*/
+
+/* %%%%%%%%%%%%%%%%%%%%%% PerspectiveCamera %%%%%%%%%%%%%%%%%%%%%%%
+* is a type of camera that mimics how the human eyes perceives objects in the real world. 
+
+- It creates a sense of "depth" and "distance" by rendering objects farther away as smaller than those closer to the camera. 
 
 - This effect is achieved through perspective projection, where objects closer to the camera appear larger and those farther away appear smaller.
 
-? PerspectiveCamera(1-fov, 2-aspect ratio, 3-camera near property, 4-camera far property)
+* PerspectiveCamera(1-fov, 2-aspect ratio, 3-camera near property, 4-camera far property)
 
-1. Field of View (FOV): Camera frustum vertical field of view.
+?1. Field of View (FOV): Camera frustum{the viewing volume} vertical field of view.
   - how much of the scene is visible to the camera. 
   - A wider FOV captures more of the scene, 
   - while a narrower FOV zooms in on specific details. 
   - It is typically measured in "degrees" and can greatly impact the perception of depth and spatial relationships within the rendered image.
 
-2. Aspect Ratio: defines the proportional relationship between the width and height of the camera's view. 
+?2. Aspect Ratio: defines the proportional relationship between the width and height of the camera's view. 
   - It ensures that objects appear in their correct proportions and prevents distortion in the final image. 
   - By setting the aspect ratio correctly, you can maintain the intended look of the scene without "stretching" or "compressing" objects unnaturally.
 
-3. Camera Near Property: specifies the distance from the camera at which objects will start to be rendered. 
+?3. Camera Near Property: specifies the distance from the camera at which objects will start to be rendered. 
   - Anything closer to the camera than this value will not be visible in the final image. 
   - Setting an appropriate near value is crucial for avoiding clipping or rendering issues with objects that are too close to the camera.
 
-4. Camera Far Property: determines the maximum distance from the camera at which objects will be rendered. 
+?4. Camera Far Property: determines the maximum distance from the camera at which objects will be rendered. 
   - Anything beyond this distance will not be visible in the final image. 
   - By adjusting the far property, you can control how far into the scene objects are rendered, optimizing performance and reducing unnecessary rendering of distant objects.
 */
 
-/* ======================= WebGLRenderer() =======================
-?- WebGL IS A JavaScript API for rendering 3D graphics in a web browser using the GPU
-
-- WebGL provides a language to talk to the GPU and instruct it on what to render
-?- GPU:
-  Graphic Processing Unit
-  a special type of hardware that can run simple calculations in parallel
-  designed to handle the demands of rendering 3D graphics
-
-- The WebGLRenderer is a crucial component in Three.js that enables rendering 3D graphics using WebGL, a web standard for high-performance graphics rendering.
-
-- it leverages the power of WebGL to render complex 3D scenes efficiently in a web browser. 
-- WebGL provides direct access to the GPU, allowing for hardware-accelerated rendering, which is essential for achieving smooth and visually appealing 3D graphics.
-*/
-
-/* ======================= near & far vs position =======================
+/* %%%%%%%%%%%%%%%%%%%%% near&far vs position %%%%%%%%%%%%%%%%%%%%%
 ? what is the "far-prop" that I can't see this box?
 - for example: "3" -> anything beyond 3 I can't see it
-  because if we set {far to 3} it means we only can see the box in range of 3 units, however we set the {position to 5} so the camera is more far away from over box
+  because if we set {far to 3} it means we only can see the box in range of 3 units, however we set the {position to 5} so the camera is more far away from over box so we can not see anything 
 
 ? what is the "near-prop" that I can't see this box?
-- fro example: "5" -> since we set the {position to 5} it means the camera is positioned 5 units away and that's the "center of the object" so if we set {near to 5} we can't see anything but since the object in cubeGeometry sets as 1 by 1 by 1, it still exists within 4.5 units of the actual camera
+- for example: "5" -> since we set the {position to 5} it means the camera is positioned 5 units away and that's the "center of the object" so if we set {near to 5} we can't see anything but since the object in cubeGeometry sets as 1 by 1 by 1, it still exists within 4.5 units of the actual camera
 */
 
-/* ======================= OrbitControls() ======================= 
+/* %%%%%%%%%%%%%%%%%%%%% Orbit Controls %%%%%%%%%%%%%%%%%%%%%%%%%%%
 ? OrbitControls(camera, domElement)
-- Orbit controls allow the camera to orbit(rotate) around a target.
+* Orbit controls allow the camera to orbit(rotate) around a target.
 
-? enableDamping:
+@ enableDamping:
 - refers to a mechanism that introduces a gradual decrease in a certain parameter or behavior over time.
 
-- When damping is enabled, it introduces a smoothing effect to the movement of the camera controlled by OrbitControls. 
+- When damping is enabled, it introduces a "smoothing effect to the movement" of the camera controlled by OrbitControls. 
+
 - This can be particularly useful when you want to avoid abrupt or jerky movements, providing a more fluid and natural interaction experience for users.
+
+01. import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+02. instantiate "control"          camera  DOM-Element
+  const control = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+  controls.autoRotate = true;
+
+
+03. call & update method
+  controls.update();
+  
+- it updates the state of the controls, including any damping effects or auto-rotation settings that have been configured.
 */
 
-/* ======================= requestAnimationFrame() ======================= 
-? - 60frames per second experience
-- used in web development to optimize animations and improve performance by synchronizing them with the browser's repaint cycle.
+/* %%%%%%%%%%%% requestAnimationFrame & renderLoop %%%%%%%%%%%%%%% 
+* "requestAnimationFrame" is used in web development to optimize animations and improve performance by synchronizing them with the browser's repaint cycle.
+? 60frames per second experience
 
-- is a method provided by the browser that allows developers to schedule an animation frame to be rendered on the next repaint cycle of the browser. 
+- is a method [provided by the browser] that allows developers to schedule an animation frame to be rendered on the next repaint cycle of the browser. 
+
 - This is crucial for creating smooth and efficient animations on web pages as it ensures that the animations are synchronized with the display refresh rate, leading to better performance and visual quality.
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const renderLoop = () => {
+* controls.update();
+* renderer.render(scene, camera);
+* window.requestAnimationFrame(renderLoop);
+/->wait until you're able to produce this frame and then call myself again to create a continuous animation loop.
+};
+
+renderLoop();
 */
 
-/* ===================== OrthographicCamera() ========================
-- Orthographic cameras are defined by a rectangular frustum (the viewing volume) 
-?- OrthographicCamera(left, right, top, bottom, near, far)
-
- const aspectRatio = window.innerWidth / window.innerHeight;
- const camera = new THREE.OrthographicCamera(
-   -1 * aspectRatio,
-   1 * aspectRatio,
-   1,
-   -1,
-   0.1,
-   200
- );
+/* %%%%%%%%%%%%%%%%%%%%% Orthographic Camera %%%%%%%%%%%%%%%%%%%%%%%
+* Orthographic cameras are defined by a rectangular frustum 
 
 - is a type of camera that is used to create a parallel projection, as opposed to a perspective projection. 
 
 - This means that objects in the scene will appear the same size regardless of their distance from the camera, which can be useful for certain types of applications such as "architectural visualization" or "2D games".
 
+? OrthographicCamera(left, right, top, bottom, near, far)
+- denoting the distance between the center of the camera and actual edges of this box(FOV)
+
+ const aspectRatio = window.innerWidth / window.innerHeight;
+ const camera = new THREE.OrthographicCamera(
+   -1 * aspectRatio,
+    1 * aspectRatio,
+    1,
+   -1,
+   0.1,
+   200
+ );
+
 ? why we multiplied left and right to aspectRatio?  
 ?       -1 * aspectRatio,  <- left
 ?        1 * aspectRatio,  <- right
--> adjusting the horizontal viewing range of the camera to match the aspect ratio of the window. 
-- This adjustment ensures that objects rendered on screen are not distorted or stretched inappropriately.
+-> adjusting the horizontal viewing range of the camera to match the aspect ratio of the window so objects rendered on screen are not distorted or stretched inappropriately.
 */
 
-/* ============ updateProjectionMatrix() & update() ==================
+/* ============ updateProjectionMatrix & update ==================
 - is used to recalculate the camera's projection matrix after any changes to the camera's parameters, such as aspect ratio or field of view. 
 - This ensures that the camera's view frustum and perspective are updated correctly to reflect the changes in the scene.
 
-?- controls.update() 
-- it updates the state of the controls, including any damping effects or auto-rotation settings that have been configured.
+?
 */
 
 /* ================== antialias & maxPixelRatio =======================
